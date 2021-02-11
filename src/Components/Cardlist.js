3868f7a38b61b0e483d654e66286a557d5ccb722
gauravmishra99 <gauravmishra.190699@gmail.com>
2021-02-11 12:50:46 +0530

    Changes to file structure and rendering the location cards according to distance

--- src/Components/Cardlist.js
+++ src/Components/Cardlist.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import Card from './Card';
-import "./Cardlist.css"
+import { React } from "react";
+import Card from "./Card";
+import "./Cardlist.css";
 // here we save data from location file but we need to import this from the database
-const CardList = ({ location }) => {
+const CardList = ({ lat, lon, locations }) => {
   return (
-    <div className = "sidebar">
-      {
-        location.map((user, i) => {
+    <div className="sidebar">
+      {console.log(locations)}
+      {locations
+        .sort((a, b) => (a.d > b.d ? 1 : -1))
+        .map((location) => {
+          console.log(location.d);
           return (
-            <Card
-              key={i}
-              id={location[i].id}
-              name={location[i].name}
-              
-              />
+            <Card key={location.id} id={location.id} name={location.name} />
           );
-        })
-      }
+        })}
     </div>
   );
-}
+};
 
 export default CardList;
--- src/Container/Admin.js
+++ src/Container/Admin.js
@@ -1,59 +1,44 @@
-import React ,{Component} from 'react' 
-import {Redirect} from "react-router-dom"
-import Navbar from "./Navbar"
-import Map from "./Map"
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import Navbar from "./Navbar";
+import Map from "./Map";
 
+class Admin extends Component {
+  constructor() {
+    super();
+    let a = "";
+    let data = localStorage.getItem("mydata");
+    data = JSON.parse(data);
 
-class Admin extends Component{
-    constructor(){
-        super()
-        let a = ""
-        let data = localStorage.getItem("mydata")
-        data = JSON.parse(data)
-        
-        if(data !== null){
-            a = "admin"
-        }
-
-
-
-        this.state = {
-            test : a 
-
-        }
-    }
-    logout = () =>{
-        localStorage.removeItem("mydata")
-        this.setState({test : "logout"})
-
+    if (data !== null) {
+      a = "admin";
     }
 
+    this.state = {
+      test: a,
+    };
+  }
+  logout = () => {
+    localStorage.removeItem("mydata");
+    this.setState({ test: "logout" });
+  };
 
-    render(){
-        if(this.state.test === "admin"){
-            return(
-                <div>
-                    {/* <button onClick = {this.logout} className = "btn btn-danger" style={{display: 'flex', justifyContent: 'flex-end'}} >logout</button>                     */}
-                    <Navbar logout = {this.logout}/>
-                    <Map/>
-                    
-
-                </div>
-              
-            )
-
-        }
-        else if(this.state.test === "logout"){
-            return(
-                <Redirect to = "/"/>
-            )
-
-        }
-        // else{
-        //     <Board/>
-        // }
-        
+  render() {
+    if (this.state.test === "admin") {
+      return (
+        <div>
+          {/* <button onClick = {this.logout} className = "btn btn-danger" style={{display: 'flex', justifyContent: 'flex-end'}} >logout</button>                     */}
+          <Navbar logout={this.logout} />
+          <Map />
+        </div>
+      );
+    } else if (this.state.test === "logout") {
+      return <Redirect to="/" />;
     }
-} 
+    // else{
+    //     <Board/>
+    // }
+  }
+}
 
-export default Admin 
+export default Admin;
